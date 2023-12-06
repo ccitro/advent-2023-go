7a7f403bd2e673129aff4faf672ac268c92bd715
Craig Citro <craig@citro.net>
2023-12-06 07:26:21 -0600

    Mac compat

--- init-day.sh
+++ init-day.sh
@@ -19,17 +19,21 @@ GO_FILE="$DAY_DIR/day$DAY_NUM.go"
 mv $DAY_DIR/dayXX.go $GO_FILE
 
 # Update package name in the Go file
-sed -i "s/package dayXX/package day$DAY_NUM/" $GO_FILE
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS requires an explicit argument for in-place editing
+  sed -i '' "s/package dayXX/package day$DAY_NUM/" $GO_FILE
+else
+  # Linux
+  sed -i "s/package dayXX/package day$DAY_NUM/" $GO_FILE
+fi
 
 # Add import statement to main.go
-IMPORT_STATEMENT="\t\"github.com/ccitro/advent-2023-go/$DAY_DIR\""
-sed -i "/import (/a\\
-$IMPORT_STATEMENT" main.go
+IMPORT_STATEMENT="\"github.com/ccitro/advent-2023-go/$DAY_DIR\""
+awk -v s="$IMPORT_STATEMENT" '/import \(/ { print; print "\t"s; next }1' main.go > tmp.go && mv tmp.go main.go
 
 # Add entry to the dayMethods map
-MAP_ENTRY="\t\"day$DAY_NUM\": {LoadPuzzle: day$DAY_NUM.LoadPuzzle, Part1: day$DAY_NUM.Part1, Part2: day$DAY_NUM.Part2},"
-sed -i "/var dayMethods = map\[string\]DayMethods{/a\\
-$MAP_ENTRY" main.go
+MAP_ENTRY="\"day$DAY_NUM\": {LoadPuzzle: day$DAY_NUM.LoadPuzzle, Part1: day$DAY_NUM.Part1, Part2: day$DAY_NUM.Part2},"
+awk -v m="$MAP_ENTRY" '/var dayMethods = map\[string\]DayMethods{/ { print; print "\t"m; next }1' main.go > tmp.go && mv tmp.go main.go
 
 # Format main.go to order the imports
 go fmt main.go
